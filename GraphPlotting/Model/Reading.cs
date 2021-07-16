8d76fec9024846f481b0de1392982a0e5d6c8c6b
haowu <hao.wu.au@gmail.com>
2021-07-16 22:57:48 +1000

    Added ScottPlot nuget for chart.

---
+++ GraphPlotting/Model/Reading.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphPlotting.Model
+{
+    class Reading
+    {
+        public int SignalStrength { get; set; }
+
+        public int PulseWaveform { get; set; }
+
+        public int Pulse { get; set; }
+
+        public int Spo2 { get; set; }
+    }
+}
