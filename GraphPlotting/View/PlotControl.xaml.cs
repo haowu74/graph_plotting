2d00e33c1310b6eb5fd578ee3b0a34379e5430f1
haowu <hao.wu.au@gmail.com>
2021-07-18 13:17:01 +1000

    Create UserControl for each Plot panel.

---
+++ GraphPlotting/View/PlotControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿using GraphPlotting.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraphPlotting.View
+{
+    /// <summary>
+    /// Interaction logic for PlotControl.xaml
+    /// </summary>
+    public partial class PlotControl : UserControl
+    {
+        private PlotVM viewModel;
+
+        public object Id 
+        { 
+            get
+            {
+                return viewModel.Id;
+            }
+            set 
+            {
+                viewModel.Id = value;
+            } 
+        }
+
+        public static readonly DependencyProperty IdProperty =
+            DependencyProperty.Register("Id", typeof(string),
+            typeof(PlotControl), new PropertyMetadata(""));
+
+        public PlotControl()
+        {
+            viewModel = new PlotVM();
+            InitializeComponent();
+        }
+    }
+}
---
+++ GraphPlotting/ViewModel/PlotVM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphPlotting.ViewModel
+{
+    public class PlotVM: INotifyPropertyChanged
+    {
+        private int id;
+        public object Id 
+        { 
+            get { return (object)id; } 
+            set 
+            {
+                id = (int)value;
+                OnPropertyChanged("Id");
+            } 
+        }
+
+        public PlotVM()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
